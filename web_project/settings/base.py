3340b0c792f17cf359330557ea8b8facc38e642e
Karl Wooster <karl@woostertech.com>
2022-10-11 15:13:41 -0700

    fix: correct default allowed_hosts value
    wasn't a list

--- web_project/settings/base.py
+++ web_project/settings/base.py
@@ -63,7 +63,7 @@ logging.debug(f"last 4 of secret key: {SECRET_KEY[-4:]}")
 
 DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default="127.0.0.1")
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["127.0.0.1"])
 
 logging.debug(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}")
 
