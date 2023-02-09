c529a59521f14610d2df3a3163dfdc432c1e281c
Karl Wooster <karl@woostertech.com>
2023-02-08 21:12:41 -0800

    feat: allow `null` for account and budget

--- accounts/models.py
+++ accounts/models.py
@@ -17,14 +17,13 @@ class Account(BaseModel):
         max_length=200,
     )
     account = models.CharField(
-        _("account"),
-        help_text=_("in form XXXX-XXXX."),
-        max_length=10,
+        _("account"), help_text=_("in form XXXX-XXXX."), max_length=10, null=True
     )
     budget_code = models.CharField(
         _("budget code"),
         help_text=_("usually first four characters of account"),
         max_length=5,
+        null=True,
     )
 
     class FundType(models.TextChoices):
