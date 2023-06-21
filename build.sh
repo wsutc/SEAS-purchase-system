18e8baaad1991eb0dd9c2a805673c8647433676e
karl.wooster <karl.wooster@wsu.edu>
2023-06-21 01:31:38 -0700

    docs: add migration comment

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ pip install -r requirements/production.txt
 
 python manage.py collectstatic --no-input
 
+# Fakes migration to use custom `user` model as well as other changes. Only needs to be run on dbs that had previous migrations
+# and are switching mid-stream. New dbs will handle everything with standard (existing) migrations.
 python -m manage dbshell "INSERT INTO django_migrations (app, name, applied) VALUES ('users', '0001_initial', CURRENT_TIMESTAMP);"
 python -m manage dbshell "UPDATE django_content_type SET app_label = 'users' WHERE app_label = 'auth' and model = 'user';"
 
