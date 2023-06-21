532105081d3d5880d3179db2ae6cb155564ec56e
Karl Wooster <karl@woostertech.com>
2023-06-20 23:44:42 -0700

    build: refactor dbshell calls

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ pip install -r requirements/production.txt
 
 python manage.py collectstatic --no-input
 
-echo "INSERT INTO django_migrations (app, name, applied) VALUES ('users', 0001_initial', CURRENT_TIMESTAMP);" | python manage.py dbshell
-echo "UPDATE django_content_type SET app_label = 'users' WHERE app_label = 'auth' and model = 'user';" | python manage.py dbshell
+python -m manage dbshell "INSERT INTO django_migrations (app, name, applied) VALUES ('users', '0001_initial', CURRENT_TIMESTAMP);"
+python -m manage dbshell "UPDATE django_content_type SET app_label = 'users' WHERE app_label = 'auth' and model = 'user';"
 
 python manage.py migrate
