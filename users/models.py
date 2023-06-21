9e268d636d59ac736d76dbcd58ffee8d7ef10f68
Karl Wooster <karl@woostertech.com>
2023-06-21 00:29:41 -0700

    db: rename user table to default

--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 
 # from django.db import models
+# from django.utils.translation import gettext_lazy as _
+# from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
-    class Meta:
-        db_table = "auth_user"
+    pass
