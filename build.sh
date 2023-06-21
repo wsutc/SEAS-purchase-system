891cb21d15f70ebec901af499cdc300b53388bdd
Karl Wooster <karl@woostertech.com>
2023-06-20 23:14:24 -0700

    build: add manual migrations

--- build.sh
+++ build.sh
@@ -7,4 +7,8 @@ pip install --upgrade pip
 pip install -r requirements/production.txt
 
 python manage.py collectstatic --no-input
+
+echo "INSERT INTO django_migrations (app, name, applied) VALUES ('users', 0001_initial', CURRENT_TIMESTAMP);" | python manage.py dbshell
+echo "UPDATE django_content_type SET app_label = 'users' WHERE app_label = 'auth' and model = 'user';" | python manage.py dbshell
+
 python manage.py migrate
