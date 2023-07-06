28cc0d3287271fef55fa3d27a07c22b0581f45d5
karl.wooster <karl.wooster@wsu.edu>
2023-07-06 08:11:06 -0700

    feat: add "Building" to "Room"
    remove `CACHES` from base settings to use default except in production

--- web_project/settings/base.py
+++ web_project/settings/base.py
@@ -209,23 +209,23 @@ DATABASES = {
     },
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "IGNORE_EXCEPTIONS": True,
-        },
-    },
-    # "select2": {
-    #     "BACKEND": "django_redis.cache.RedisCache",
-    #     "LOCATION": "redis://127.0.0.1:6379/2",
-    #     "OPTIONS": {
-    #         "CLIENT_CLASS": "django_redis.client.DefaultClient",
-    #     },
-    # },
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/2",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#             "IGNORE_EXCEPTIONS": True,
+#         },
+#     },
+#     # "select2": {
+#     #     "BACKEND": "django_redis.cache.RedisCache",
+#     #     "LOCATION": "redis://127.0.0.1:6379/2",
+#     #     "OPTIONS": {
+#     #         "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#     #     },
+#     # },
+# }
 
 # DATABASES = {
 #     "default": {
